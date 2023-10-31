1c5b18cb7fba53d9237336d7699ae537d70df6ac
rajan Mahadevan <rajanchakravarthy@gmail.com>
2023-10-31 20:58:44 +0100

    implement Spa Reservation

---
+++ README.md
@@ -0,0 +1,4 @@
+# What is the function of this app?
+
+This is a basic Hotel Booking app involving OOP concept including functions to book a hotel, make and print reservation. 
+Also has a functions to validate and Authenticate a Credit Card details stored in a dataframe. 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Reading all the csv files into dataframes
 df = pd.read_csv('hotels.csv', dtype={'id': str})
 df_cards = pd.read_csv('cards.csv', dtype=str).to_dict(orient='records')
 df_cards_secure = pd.read_csv('card_security.csv', dtype=str)
@@ -26,12 +27,18 @@ class Hotel():
             return False
 
 
+class Spa(Hotel):
+    def book_spa_package(self):
+        pass
+
+
 class ReservationTicket():
     def __init__(self, customer_name, hotel_object):
         self.customer_name = customer_name
         self.hotel_object = hotel_object
 
     def generate(self):
+        ''' Generates a bookings reservation '''
         content = f'''
         
         Thank you for your reservation!
@@ -48,6 +55,7 @@ class CreditCard():
         self.number = number
 
     def validate(self, expiration, cvc, holder):
+        ''' Validates the credit card details with the df_cards'''
         card_dict = {'number': self.number, 'expiration': expiration, 'cvc': cvc, 'holder': holder}
         if card_dict in df_cards:
             return True
@@ -57,23 +65,50 @@ class CreditCard():
 
 class SecureCreditCard(CreditCard):
     def authenticate(self, password):
+        ''' Authenticates the creditcard with its password. '''
         pass_w = df_cards_secure.loc[df_cards_secure['number'] == self.number, ['password']].squeeze()
         if pass_w == password:
             return True
 
 
+class SpaReservation():
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel_object = hotel_object
+
+    def generate(self):
+        ''' Generates the SPA Reservation '''
+        content = f'''
+
+        Thank you for your SPA reservation!
+        Here are you SPA booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel_object.hotel_name}
+
+        '''
+        return content
+
+# List all the hotels
 print(df)
 hotel_id = input('Enter the id of the hotel: ')
 hotel = Hotel(hotel_id)
+# Checks hotel availability
 if hotel.available():
     creditcard = SecureCreditCard(number='1234')
+    # Validates the Credit Card
     if creditcard.validate(expiration='12/26', cvc='123', holder='JOHN SMITH'):
-        if creditcard.authenticate(password='mypass1'):
+        # Authenticates the card with its password
+        if creditcard.authenticate(password='mypass'):
             print('Credit card authenticated.')
             hotel.book()
             name = input('Enter your name: ')
-            reservation_ticket = ReservationTicket(customer_name= name, hotel_object=hotel)
+            reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
             print(reservation_ticket.generate())
+            spa_choice = input('Do you want to book a spa package?')
+            # Prints Spa reservation when notified.
+            if spa_choice == 'yes':
+                spa_reservation = SpaReservation(customer_name=name, hotel_object=hotel)
+                print(spa_reservation.generate())
         else:
             print('Credit Card not Authenticated.')
     else:
